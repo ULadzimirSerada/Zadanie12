fb74925f4684a089f93a73edadc9b596ce3e9d04
UladzimirSerada <medavikiby@gmail.com>
2021-02-05 19:29:42 +0300

    (Z12)

---
+++ Zadanie12.java
@@ -0,0 +1,17 @@
+        public class Zadanie12 {
+    public static void main(String[] args) {
+        int n =10;
+        int k = 1;
+        int sum = 0;
+        for (int i = 1; k <= n; i++) {
+            if (i % 3 == 0)
+           if (n != 0) {
+               sum += i;
+
+                    System.out.println("Сумма чисел, которые делится на 3 равно" + sum);
+                    k++;
+                }
+        }
+
+    }
+}
